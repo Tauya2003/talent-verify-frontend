bd74159e5d9f9b9b51ff7bcea6cc77f906315820
Tauya Mtowodzwa <ayuat2003@gmail.com>
2024-06-23 16:30:11 +0200

    ui(layout): Add icons to sidebar items

--- src/components/CollapsibleMenu.jsx
+++ src/components/CollapsibleMenu.jsx
@@ -17,28 +17,27 @@ const CollapsibleMenu = ({ index, name, items, isOpen, updateMenuState }) => {
       </Button>
 
       <Collapse in={isOpen}>
-        <ul style={{ paddingLeft: "2rem" }}>
-          {items.map((item, index) => (
-            <li key={index}>
-              <Button
-                sx={{
-                  textTransform: "capitalize",
-                  color: "#444",
-                  width: "100%",
-                  fontSize: ".75rem",
-                  justifyContent: "flex-start",
+        {items.map((item, index) => (
+          <Button
+            key={index}
+            startIcon={item?.icon}
+            sx={{
+              textTransform: "capitalize",
+              color: "#444",
+              width: "100%",
+              fontSize: ".75rem",
+              justifyContent: "flex-start",
+              pl: 3,
 
-                  "&:hover": {
-                    bgcolor: "transparent",
-                    color: "hsl(0, 0%, 8%)",
-                  },
-                }}
-              >
-                {item.name}
-              </Button>
-            </li>
-          ))}
-        </ul>
+              "&:hover": {
+                bgcolor: "transparent",
+                color: "hsl(0, 0%, 8%)",
+              },
+            }}
+          >
+            {item.name}
+          </Button>
+        ))}
       </Collapse>
     </div>
   );
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,32 +1,100 @@
+import {
+  AdminPanelSettingsTwoTone,
+  BadgeTwoTone,
+  BusinessTwoTone,
+  DriveFolderUploadTwoTone,
+  FormatListBulletedTwoTone,
+  SearchTwoTone,
+  SecurityTwoTone,
+  SettingsAccessibilityTwoTone,
+  SummarizeTwoTone,
+  UploadFileTwoTone,
+  WorkHistoryTwoTone,
+} from "@mui/icons-material";
+
 export const menuItems = [
   {
     name: "Employee Management",
     items: [
-      { name: "Employee List" },
-      { name: "Employee Details" },
-      { name: "Employee Search" },
+      {
+        name: "Employee List",
+        icon: <FormatListBulletedTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+      {
+        name: "Employee Details",
+        icon: <BadgeTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+      {
+        name: "Employee Search",
+        icon: <SearchTwoTone sx={{ color: "#4CAF50" }} />,
+      },
     ],
     isOpen: false,
   },
   {
     name: "Company Management",
-    items: [{ name: "Company Profile" }, { name: "Company Settings" }],
+    items: [
+      {
+        name: "Company Profile",
+        icon: <BusinessTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+      {
+        name: "Company Settings",
+        icon: <AdminPanelSettingsTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+    ],
     isOpen: false,
   },
   {
     name: "Data Management",
-    items: [{ name: "Bulk Upload" }, { name: "Single Entry" }],
+    items: [
+      {
+        name: "Bulk Upload",
+        icon: <DriveFolderUploadTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+      {
+        name: "Single Entry",
+        icon: <UploadFileTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+    ],
     isOpen: false,
   },
-  { name: "History", items: [{ name: "Employee History" }], isOpen: false },
+  {
+    name: "History",
+    items: [
+      {
+        name: "Employee History",
+        icon: <WorkHistoryTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+    ],
+    isOpen: false,
+  },
   {
     name: "Settings",
-    items: [{ name: "User Preferences" }, { name: "Security Settings" }],
+    items: [
+      {
+        name: "User Preferences",
+        icon: <SettingsAccessibilityTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+      {
+        name: "Security Settings",
+        icon: <SecurityTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+    ],
     isOpen: false,
   },
   {
     name: "Reports",
-    items: [{ name: "Employee Reports" }, { name: "Company Reports" }],
+    items: [
+      {
+        name: "Employee Reports",
+        icon: <SummarizeTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+      {
+        name: "Company Reports",
+        icon: <SummarizeTwoTone sx={{ color: "#4CAF50" }} />,
+      },
+    ],
     isOpen: false,
   },
 ];
