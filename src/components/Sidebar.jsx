bb141d55f7d3b01b7d505e16b73f935f920dfc8a
Tauya Mtowodzwa <ayuat2003@gmail.com>
2024-06-24 06:39:19 +0200

    Update Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Collapse, Stack } from "@mui/material";
+import { Box, Button, Collapse, Stack, Typography } from "@mui/material";
 import React, { useState } from "react";
 import CollapsibleMenu from "./CollapsibleMenu";
 import { menuItems } from "../utils/constants";
@@ -9,6 +9,12 @@ const Sidebar = () => {
   const updateMenuState = (index) => {
     setMenus((prevMenus) => {
       const updatedMenus = [...prevMenus];
+      let currentmenu = updatedMenus[index];
+      updatedMenus.forEach((menu) => {
+        if (menu !== currentmenu) {
+          menu.isOpen = false;
+        }
+      });
       updatedMenus[index].isOpen = !prevMenus[index].isOpen;
       return updatedMenus;
     });
@@ -21,18 +27,49 @@ const Sidebar = () => {
         height: "100vh",
         bgcolor: "#f9f9f9",
         p: "4rem 1rem 2rem 1rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        position: "fixed",
       }}
     >
-      <Stack sx={{ width: "100%" }}>
-        {menus.map((item, index) => (
-          <CollapsibleMenu
-            key={index}
-            index={index}
-            updateMenuState={updateMenuState}
-            {...item}
-          />
-        ))}
-      </Stack>
+      <span>
+        <Typography
+          sx={{
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+            mb: "2rem",
+            textAlign: "center",
+          }}
+        >
+          Talent Verify
+        </Typography>
+
+        <Stack sx={{ width: "100%" }}>
+          {menus.map((item, index) => (
+            <CollapsibleMenu
+              key={index}
+              index={index}
+              updateMenuState={updateMenuState}
+              {...item}
+            />
+          ))}
+        </Stack>
+      </span>
+
+      <Button
+        sx={{
+          width: "100%",
+          color: "#000",
+          bgcolor: "#ccc",
+
+          "&:hover": {
+            bgcolor: "#ddd",
+          },
+        }}
+      >
+        Logout
+      </Button>
     </Box>
   );
 };
