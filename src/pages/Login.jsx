b8b6e830d07568c15b1c9198e9f8f0695a28224f
Tauya Mtowodzwa <ayuat2003@gmail.com>
2024-07-11 12:44:12 +0200

    update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,6 @@ import { CheckBox } from "@mui/icons-material";
 import { useContext, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import dashboard from "../assets/images/dashboard.png";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -37,14 +36,38 @@ const Login = () => {
     <Box sx={{ display: "flex", height: "100vh", width: "100vw" }}>
       <Box
         sx={{
+          position: "relative",
+          overflow: "hidden",
           width: "60%",
           height: "100%",
-          backgroundImage: `url(${dashboard})`,
-          backgroundSize: "70%",
-          backgroundPosition: "102% 50%",
-          backgroundRepeat: "no-repeat",
+          background:
+            "linear-gradient(180deg, #7152F320 0%, #5A3DC420 87%, #4A2E8D50 100%)",
         }}
-      />
+      >
+        <Box
+          sx={{
+            width: "557px",
+            height: "557px",
+            border: "1px solid #16151C",
+            borderRadius: "50%",
+            position: "absolute",
+            bottom: "-300px",
+            left: "-250px",
+          }}
+        />
+
+        <Box
+          sx={{
+            width: "557px",
+            height: "557px",
+            border: "1px solid #16151C",
+            borderRadius: "50%",
+            position: "absolute",
+            bottom: "-326px",
+            left: "-169px",
+          }}
+        />
+      </Box>
 
       <Box
         sx={{
@@ -52,6 +75,7 @@ const Login = () => {
           width: "40%",
           height: "100%",
           backgroundColor: "#16151C",
+          // backgroundColor: "#4A2E8D",
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
